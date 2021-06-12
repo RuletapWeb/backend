c66b9b0bd997be116522ead589c9e3cbad58f8a0
lautaro <lautaro.baltar@azumo.co>
2021-06-12 05:45:01 -0300

    endpoint prize/winner con validacion y creacion de reward

--- app/api/prizes/controllers/prizes.js
+++ app/api/prizes/controllers/prizes.js
@@ -5,22 +5,60 @@ function getRandomArbitrary(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
+function makeToken(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+async function craeteReward(user, prize){
+    email = prize["shop"]["email"]
+    shop = await strapi.services.shop.findOne({ email });
+    body = {
+        "player": user,
+        "shop": shop,
+        "prize": prize,
+        "token": makeToken(10),
+    };
+    return strapi.query('reward').create(body)
+}
+
 module.exports = {
     async getWinner(ctx) {
+        if(ctx.query["email"]){
+            email = ctx.query["email"];
+            user = await strapi.services.player.findOne({ email });
+        } else {
+            return { message: "Missing user email", code: 400}
+        }
         let entities;
         entities = await strapi.services.prizes.find()
         // Create array based in each prize probability
         prize_pool = []
         entities.forEach(
             function(currentValue, currentIndex, listObj) {
-              console.log("Name: " + currentValue["title"]);
-              console.log("Probability: " + currentValue["probability"]);
-              for ( i = 0; i < currentValue["probability"]; i++){
-                  prize_pool.push(currentValue);
-              }
-              console.log("Pool Size: " + prize_pool.length);
+                if(currentValue["quantityLeft"] > 0){
+                    for ( i = 0; i < currentValue["probability"]; i++){
+                            prize_pool.push(currentValue);
+                    }
+                }
             });
-        winner = prize_pool[getRandomArbitrary(0,prize_pool.length)]
-        return sanitizeEntity(winner, { model: strapi.models.prizes });
+        if(prize_pool.length > 1 ){
+            winner = prize_pool[getRandomArbitrary(0,prize_pool.length)];
+            await strapi.query('prizes').update(
+                { id: winner["id"]},
+                {
+                    quantityLeft: (winner["quantityLeft"] -1 ),
+                }    
+            )
+            reward = await craeteReward(user,winner)
+            return sanitizeEntity(reward, { model: strapi.models.reward });
+        } else {
+            return { message: "No prizes available", code: 404};
+        }
       },
 };
