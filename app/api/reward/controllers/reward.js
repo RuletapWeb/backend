83c5af6ab9dc1d611f4b0c9d02f834c0f60412c7
lautaro <lautaro.baltar@azumo.co>
2021-06-13 19:25:53 -0300

    find reward by user

--- app/api/reward/controllers/reward.js
+++ app/api/reward/controllers/reward.js
@@ -5,4 +5,38 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async findByUser(ctx) {
+        if(ctx.query["email"]){
+            email = ctx.query["email"];
+            user = await strapi.services.player.findOne({ email });
+        } else {
+            return { message: "Missing user email", code: 400}
+        }
+        let entities;
+        entities = await strapi.services.prizes.find()
+        // Create array based in each prize probability
+        prize_pool = []
+        entities.forEach(
+            function(currentValue, currentIndex, listObj) {
+                if(currentValue["quantityLeft"] > 0){
+                    for ( i = 0; i < currentValue["probability"]; i++){
+                            prize_pool.push(currentValue);
+                    }
+                }
+            });
+        if(prize_pool.length > 1 ){
+            winner = prize_pool[getRandomArbitrary(0,prize_pool.length)];
+            await strapi.query('prizes').update(
+                { id: winner["id"]},
+                {
+                    quantityLeft: (winner["quantityLeft"] -1 ),
+                }    
+            )
+            reward = await craeteReward(user,winner)
+            return sanitizeEntity(reward, { model: strapi.models.reward });
+        } else {
+            return { message: "No prizes available", code: 404};
+        }
+      },
+};
